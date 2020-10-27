b43a5b51b0b16e1b87a460be6ad73a560dd724b8
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-10-28 00:31:36 +0900

    main 화면 전환 x

---
+++ Main/choice.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+
+from Diary import Diary
+
+class Choice(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btnDiary=QPushButton('일기 쓰기', self)
+        btnGame=QPushButton('맘마 먹자',self)
+
+        btnDiary.move(100,100)
+        btnDiary.resize(300,300)
+        btnDiary.clicked.connect(self.GoDiary)
+
+        btnGame.move(500, 100)
+        btnGame.resize(300, 300)
+        # btnGame.clicked.connect(self.GoGame)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(btnDiary)
+        vbox.addWidget(btnGame)
+        vbox.addStretch(1)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addLayout(vbox)
+        hbox.addStretch(1)
+
+        self.setLayout(hbox)
+        self.setWindowTitle('Momma')
+        self.setWindowIcon(QIcon('baby.png'))
+        self.setGeometry(300,100,1200,800)
+        self.show()
+
+    def GoDiary(self):
+        print('Diary 클릭!')
+        win=Diary()
+        r=self.win.show()
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    ex=Choice()
+    ex.show()
+    sys.exit(app.exec_())
